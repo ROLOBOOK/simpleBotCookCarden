13d61aafc328f0419c4369b82d0f0270a1db9596
rolobook@yandex.ru <rolobook@yandex.ru>
2021-08-25 14:50:34 +0300

    добавлен расчет детских порций

--- bot.py
+++ bot.py
@@ -4,63 +4,85 @@ from history import save_history
 from keyBoard import keyboard_markup
 from CookRequest import CookRequest
 from for_bd import dish_for_one, all_dish_for_one, recipes
-from calculation import calculation
+from calculation import get_portion
 from config import TOKEN
-
 
 bot = telebot.TeleBot(TOKEN)
 
+START = 'чтобы начать сначала нажмите /start'
+
+
+def make_command_recipe(chat_id, cook_request):
+    """Отправляем рецепт блюда"""
+    if cook_request.dish:
+        portion = recipes[cook_request.dish][0]
+        recipe = recipes[cook_request.dish][1]
+        bot.send_message(chat_id=chat_id, text=f'Одна порция - {portion}\nРецепт - {recipe}\n'
+                                               f'{START}')
+    else:
+        bot.send_message(chat_id=chat_id, text=f'Блюдо еще не выбрано,{START}')
+
+
+def make_command_children(chat_id, cook_request):
+    """Расчитываем ингридиенты для детских порций"""
+
+    if cook_request.dish:
+        calculat = get_portion(cook_request.dish, cook_request.count_people, children=True)
+        answer = (f'Детские порции - {cook_request.count_people}\n'
+                  f'блюдо - {cook_request.dish}\n'
+                  f'{calculat}\n'
+                  f'Получить рецепт {cook_request.dish} нажмите /recipe\n'
+                  f'{START}')
+        bot.send_message(chat_id=chat_id, text=answer)
+    else:
+        bot.send_message(chat_id=chat_id, text=f'Блюдо еще не выбрано,{START}')
+
+
+def make_command_start(chat_id, cook_request):
+    bot.send_message(chat_id=chat_id, text='выберите или введите сколько будет порций',
+                     reply_markup=keyboard_markup(range(1, 31), digit=True))
+
+
+COMMANDS = {
+    '/start': make_command_start,
+    '/recipe': make_command_recipe,
+    '/children': make_command_children
+}
+
 
 def processing_message(request_json):
-
     message = request_json['message']
     chat_id = message['chat']['id']
     if chat_id not in cook_requests:
-
         cook_requests[chat_id] = CookRequest(chat_id)
     message_text = message['text']
-    if message.get('entities') and message_text == '/start':
-        # начало работы боты, получаем количество людей
+    cook_request = cook_requests.get(chat_id)
+    if message.get('entities'):
+        # если получили команду
+        COMMANDS[message_text](chat_id, cook_request)
 
-        bot.send_message(chat_id=chat_id, text='выберите или введите сколько будет порций',
-                         reply_markup=keyboard_markup(range(1, 31), digit=True))
-    elif message.get('entities') and message_text == '/recipe':
-        # получить рецепт по указаному блюду  cook_request.dish
-        cook_request = cook_requests.get(chat_id)
-        if cook_request.dish:
-            portion = recipes[cook_request.dish][0]
-            recipe = recipes[cook_request.dish][1]
-            bot.send_message(chat_id=chat_id, text=f'Одна порция - {portion}\nРецепт - {recipe}\n'
-                                                    'чтобы начать сначала нажмите /start')
-        else:
-            bot.send_message(chat_id=chat_id, text='Блюдо еще не выбрано,чтобы начать сначала нажмите /start')
     elif message_text.isdigit():
-
-        # если ввели число - сохраняем как количество порций и делаем клавиатру из типов блюд
-        cook_request = cook_requests.get(chat_id)
+        # если ввели число (количество блюд) - сохраняем как количество порций и делаем клавиатру из типов блюд
         cook_request.count_people = int(message_text)
         types_dish = dish_for_one.keys()
         bot.send_message(chat_id=chat_id,
                          text=f'порций - {cook_request.count_people}\n'
-                              'выберите тип блюда\n чтобы начать сначала нажмите /start',
+                              f'выберите тип блюда\n {START}',
                          reply_markup=keyboard_markup(types_dish))
     else:
 
-        bot.send_message(chat_id=chat_id, text=f'не корректное сообщение, для начала работы нажмите /start ')
+        bot.send_message(chat_id=chat_id, text=f'не корректное сообщение, {START} ')
 
 
 def processing_callback_query(request_json):
-
     dish = request_json['callback_query']['data']
     chat_id = request_json['callback_query']['message']['chat']['id']
     cook_request = cook_requests.get(chat_id)
     if dish.isdigit():
         # если ввели число - сохраняем как количество порций и делаем клавиатру из типов блюд
-        cook_request = cook_requests.get(chat_id)
         cook_request.count_people = int(dish)
         bot.send_message(chat_id=chat_id, text=f'порций - {cook_request.count_people}\nвыберите блюдо',
                          reply_markup=keyboard_markup(dish_for_one.keys()))
-
     elif dish in dish_for_one:
         # если прислали тип блюда отправляем клавиатуру с блюдами
         cook_request.typy_dish = dish
@@ -76,12 +98,13 @@ def processing_callback_query(request_json):
         # если прислали нужное блюдо, отправляем расчет ингридиентов по порциям.
         if all_dish_for_one.get(dish):
             cook_request.dish = dish
-            calculat = calculation(dish, cook_request.count_people)
+            calculat = get_portion(dish, cook_request.count_people)
             answer = (f'порций - {cook_request.count_people},\n'
                       f'блюдо - {cook_request.dish}\n'
                       f'{calculat}\n'
                       f'Получить рецепт {cook_request.dish} нажмите /recipe\n'
-                      'Для начала работы нажмите /start')
+                      'Расчитать для детских порций /children\n'
+                      f'{START}')
             bot.send_message(chat_id=chat_id, text=answer)
             save_history(cook_request)  # сохраняем результат
         else:
@@ -89,5 +112,5 @@ def processing_callback_query(request_json):
                              text=f'{dish} - нет данных по этому блюду\n для начала работы нажмите /start')
 
 
-# храним объекты CookRequest
+# объекты CookRequest храним в
 cook_requests = {}
--- calculation.py
+++ calculation.py
@@ -1,8 +1,17 @@
 from for_bd import all_dish_for_one
 
 
-def calculation(name_dish, count_people):
-    return ''.join(f'{key} - {round(value * count_people, 2)}\n' for key, value in all_dish_for_one[name_dish].items())
+def get_portion(name_dish, count_people, children=False):
+
+    return ''.join(f'{key} - {calculation(value, count_people, children)}\n' for key, value in all_dish_for_one[name_dish].items())
+
+
+def calculation(value, count_people, children, ):
+    result = value * count_people
+    if children:
+        result = result / 3 * 2
+
+    return round(result, 1)
 
 
 if __name__ == '__main__':
